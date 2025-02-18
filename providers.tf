11f43aa09c7af90013440a09d011e971eef7adcb
Rado Marina <rado@gofirefly.io>
2025-02-18 11:15:40 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,12 +11,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  features {}
-  client_id                       = var.client_id
-  client_secret                   = var.client_secret
-  tenant_id                       = var.tenant_id
-  subscription_id                 = var.subscription_id
-  resource_provider_registrations = "none"
-}
